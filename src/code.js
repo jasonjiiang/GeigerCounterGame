358d443bb6b76af9eb8271281816b656a7989db8
Jason <52567513+jasonjiiang@users.noreply.github.com>
2023-04-26 15:07:18 +0100

    Goal

--- src/code.js
+++ src/code.js
@@ -4,11 +4,43 @@ function randomColours()
 
     for (let i of tiles)
     {
-        console.log(i.style.background);
         i.style.background = '#' + (0x1000000 + Math.floor(Math.random() * 0x1000000)).toString(16).substring(1);
     }
 }
 
+function gridSize()
+{
+    const size = 100;
+
+    for (let i = 0; i < size; i++)
+    {
+        const tempLi = document.createElement("li");
+        tempLi.classList.add("tile");
+        
+        document.getElementById("tiles").insertAdjacentElement("beforeend", tempLi);
+    }
+}
+
+function randomTile()
+{
+    const tiles = document.getElementsByClassName("tile");
+    const goal = tiles[Math.floor(Math.random() * tiles.length)];
+    return goal;
+}
+
+function addButton()
+{
+    const tempButton = document.createElement("button");
+    tempButton.id = "finish";
+    tempButton.setAttribute("type", "button");
+    tempButton.innerHTML = "Finish";
+
+    document.getElementById("goal").insertAdjacentElement("beforeend", tempButton);
+}
+
 window.addEventListener('load', function() {
-    randomColours()
+    gridSize();
+    randomTile().id = ("goal");
+    addButton();
+    randomColours();
 })
