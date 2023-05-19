324d206f7a32ace99b324b564ab132fa99fd1bc6
Jason <52567513+jasonjiiang@users.noreply.github.com>
2023-05-19 21:42:44 +0100

    TypeScript

---
+++ src/code.ts
@@ -0,0 +1,46 @@
+function randomColours()
+{
+    const tiles = document.getElementsByClassName("tile");
+
+    for (let i of tiles)
+    {
+        (i as HTMLDivElement).style.background = '#' + (0x1000000 + Math.floor(Math.random() * 0x1000000)).toString(16).substring(1);
+    }
+}
+
+function gridSize()
+{
+    const size = 100;
+
+    for (let i = 0; i < size; i++)
+    {
+        const tempLi = document.createElement("li");
+        tempLi.classList.add("tile");
+        
+        (document.getElementById("tiles") as HTMLDivElement).insertAdjacentElement("beforeend", tempLi);
+    }
+}
+
+function randomTile()
+{
+    const tiles = document.getElementsByClassName("tile");
+    const goal = tiles[Math.floor(Math.random() * tiles.length)];
+    return goal;
+}
+
+function addButton()
+{
+    const tempButton = document.createElement("button");
+    tempButton.id = "finish";
+    tempButton.setAttribute("type", "button");
+    tempButton.innerHTML = "Finish";
+
+    (document.getElementById("goal") as HTMLDivElement).insertAdjacentElement("beforeend", tempButton);
+}
+
+window.addEventListener('load', function() {
+    gridSize();
+    randomTile().id = ("goal");
+    addButton();
+    randomColours();
+})
